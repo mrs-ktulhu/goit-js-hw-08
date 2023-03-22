edbc28c8ed66c0c0a8aaed312b4ca7ffd537ee53
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-03-22 14:49:34 +0000

    gallery work1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Add imports above this line
+
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+const galleryImage = document.querySelector(".gallery");
+
+console.log(galleryItems);
+
+function addGallery(galleryItems) {
+    const markup = galleryItems.map(img => 
+        `<a class="gallery__item" href"${img.original}">
+        <img class="gallery__image" src="${img.preview}" alt="${img.description}"/>
+        </a>`).join('');
+        galleryImage.innerHTML = markup
+};
+addGallery(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionData: 'alt',
+});
 console.log(galleryItems);
