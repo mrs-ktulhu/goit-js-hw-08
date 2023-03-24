368d2ee410105af6a2f1958f65f26f3e701bedf2
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-03-24 14:28:49 +0000

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,29 @@
-import SimpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Add imports above this line
-
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Change code below this line
-const galleryImage = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-function addGallery(galleryItems) {
-    const markup = galleryItems.map(img => 
-        `<a class="gallery__item" href"${img.original}">
-        <img class="gallery__image" src="${img.preview}" alt="${img.description}"/>
-        </a>`).join('');
-        galleryImage.innerHTML = markup
-};
-addGallery(galleryItems);
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`
+  }).join('');
+}
 
-const gallery = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-    captionData: 'alt',
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
 });
-console.log(galleryItems);
